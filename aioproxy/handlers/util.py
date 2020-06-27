07a69f78b75bd223d025229e1cf5fc7b88b0b4ea
Gerald <gera2ld@163.com>
2020-06-27 22:51:06 +0800

    fix: shield socket error

--- aioproxy/handlers/util.py
+++ aioproxy/handlers/util.py
@@ -1,11 +1,17 @@
+import asyncio
+
 BUF_SIZE = 4096
 
 async def forward_data(reader, writer):
-    while True:
-        try:
-            chunk = await reader.read(BUF_SIZE)
-        except ConnectionResetError:
-            break
-        if not chunk:
-            break
-        await writer.write(chunk)
+    try:
+        while True:
+            try:
+                chunk = await reader.read(BUF_SIZE)
+            except ConnectionResetError:
+                break
+            if not chunk:
+                break
+            # https://github.com/aio-libs/aiohttp/issues/3122#issuecomment-567960647
+            await asyncio.shield(writer.write(chunk))
+    finally:
+        await writer.write_eof()
