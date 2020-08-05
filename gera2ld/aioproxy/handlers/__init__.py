da99b3df8c174fa3a968e23fee24515fa1a7950f
Gerald <gera2ld@163.com>
2020-08-05 17:32:48 +0800

    fix: save connections to avoid gc

--- gera2ld/aioproxy/handlers/__init__.py
+++ gera2ld/aioproxy/handlers/__init__.py
@@ -1,14 +1,14 @@
 from gera2ld.socks.server.config import Config
-from .http import handle as handle_http
-from .socks import handle as handle_socks
+from .http import HTTPProxyHandler
+from .socks import SOCKSProxyHandler
 
 def create_handler(socks_proxy=None):
     config = Config()
     config.set_proxies([(None, socks_proxy)])
+    http_handle = HTTPProxyHandler(socks_proxy)
+    socks_handle = SOCKSProxyHandler(config)
     async def handle(reader, writer):
         feed = await reader.readexactly(1)
-        if feed in b'\4\5':
-            await handle_socks(reader, writer, config, feed)
-        else:
-            await handle_http(reader, writer, socks_proxy, feed)
+        handle_request = socks_handle if feed in b'\4\5' else http_handle
+        await handle_request(reader, writer, feed)
     return handle
--- gera2ld/aioproxy/handlers/http.py
+++ gera2ld/aioproxy/handlers/http.py
@@ -1,14 +1,17 @@
 import asyncio
 import logging
+import io
 import time
 from urllib import parse
 from gera2ld.pyserve import Host
 from gera2ld.socks.client import create_client
 from gera2ld.socks.utils import forward_pipes
 
+logger = logging.getLogger(__package__)
+
 async def read_headers(reader):
     while True:
-        line = await reader.readline()
+        line = await reader.readuntil(b'\n')
         if line == b'\r\n' or not line: break
         yield line
 
@@ -41,68 +44,91 @@ async def send_response(writer, protocol=b'HTTP/1.1', status=200, message=b'OK',
     writer.write(b'\r\n')
     await writer.drain()
 
-async def handle(reader, writer, socks_proxy=None, feed=b''):
-    first_line = feed + await reader.readline()
-    initial_len = len(first_line)
-    method, path, protocol = first_line.strip().split(b' ', 3)
-    headers = []
-    async for line in read_headers(reader):
-        initial_len += len(line)
-        key, _, value = line.strip().partition(b':')
-        if value is None: value = b''
-        headers.append((key.strip().lower(), value.strip()))
-    start_time = time.time()
-    path = path.decode()
-    try:
-        if method == b'CONNECT':
-            len_local, len_remote = await handle_connect(reader, writer, method, path, protocol, headers, socks_proxy)
-        else:
-            len_local, len_remote = await handle_proxy(reader, writer, method, path, protocol, headers, socks_proxy)
-    except:
-        len_local = len_remote = 0
-        writer.close()
-    proxy_log = ' X' + socks_proxy if socks_proxy else ''
-    len_local += initial_len
-    logging.info('%s %s%s %.3fs <%d >%d',
-            method.decode(), path,
-            proxy_log, time.time() - start_time, len_local, len_remote)
+class HTTPProxyRequest:
+    def __init__(self, handler, reader, writer):
+        self.reader = reader
+        self.writer = writer
+        self.handler = handler
+        self.remote = None
 
-async def open_connection(hostname, port, socks_proxy):
-    if socks_proxy is None:
-        remote_reader, remote_writer = await asyncio.open_connection(hostname, port)
-        return remote_reader, remote_writer
-    client = create_client(socks_proxy, remote_dns=True)
-    await client.handle_connect((hostname, port))
-    return client.reader, client.writer
+    async def handle(self, feed=b''):
+        reader = self.reader
+        writer = self.writer
+        first_line = feed + await reader.readuntil(b'\n')
+        initial_len = len(first_line)
+        method, path, protocol = first_line.strip().split(b' ', 3)
+        headers = []
+        async for line in read_headers(self.reader):
+            initial_len += len(line)
+            key, _, value = line.strip().partition(b':')
+            if value is None: value = b''
+            headers.append((key.strip().lower(), value.strip()))
+        logger.debug('headers: %s', headers)
+        start_time = time.time()
+        path = path.decode()
+        try:
+            if method == b'CONNECT':
+                len_local, len_remote = await self.handle_connect(method, path, protocol, headers)
+            else:
+                len_local, len_remote = await self.handle_proxy(method, path, protocol, headers)
+        except:
+            len_local = len_remote = 0
+        finally:
+            writer.close()
+        len_local += initial_len
+        logger.info('%s %s X%s %.3fs <%d >%d',
+                method.decode(), path,
+                self.handler.socks_proxy or '-', time.time() - start_time, len_local, len_remote)
 
-async def handle_connect(reader, writer, method, path, protocol, headers, socks_proxy):
-    proxy_log = ' X' + socks_proxy if socks_proxy else ''
-    hostinfo = Host(path)
-    logging.debug('%s %s%s', method.decode(), hostinfo.host, proxy_log)
-    await send_response(writer, protocol, message=b'Connection established')
-    remote_reader, remote_writer = await open_connection(hostinfo.hostname, hostinfo.port, socks_proxy)
-    len_local, len_remote = await forward_pipes(reader, writer, remote_reader, remote_writer)
-    return len_local, len_remote
+    async def open_connection(self, hostname, port):
+        if self.handler.socks_proxy is None:
+            logger.debug('request direct: %s:%s', hostname, port)
+            self.remote = await asyncio.open_connection(hostname, port)
+        logger.debug('request proxy: %s', self.handler.socks_proxy)
+        client = create_client(self.handler.socks_proxy, remote_dns=True)
+        await client.handle_connect((hostname, port))
+        self.remote = client.reader, client.writer
 
-async def handle_proxy(reader, writer, method, path, protocol, headers, socks_proxy):
-    proxy_log = ' X' + socks_proxy if socks_proxy else ''
-    url = parse.urlparse(path)
-    assert url.scheme == 'http'
-    hostname = url.hostname
-    port = url.port or 80
-    pathname = url.path
-    if url.query:
-        pathname += '?' + url.query
-    hostinfo = Host((hostname, port))
-    logging.debug('%s %s%s', method.decode(), hostinfo.host, proxy_log)
-    remote_reader, remote_writer = await open_connection(hostinfo.hostname, hostinfo.port, socks_proxy)
-    headers = [header for header in headers if not header[0].startswith(b'proxy-')]
-    await send_request(remote_writer, pathname.encode(), method, protocol, headers)
-    len_local, len_remote = await forward_pipes(reader, writer, remote_reader, remote_writer)
-    return len_local, len_remote
+    async def handle_connect(self, method, path, protocol, headers):
+        hostinfo = Host(path)
+        logger.debug('%s %s X%s', method.decode(), hostinfo.host, self.handler.socks_proxy or '-')
+        await self.open_connection(hostinfo.hostname, hostinfo.port)
+        await send_response(self.writer, protocol, message=b'Connection established')
+        logger.debug('connection established')
+        len_local, len_remote = await forward_pipes(self.reader, self.writer, *self.remote)
+        return len_local, len_remote
+
+    async def handle_proxy(self, method, path, protocol, headers):
+        url = parse.urlparse(path)
+        assert url.scheme == 'http'
+        hostname = url.hostname
+        port = url.port or 80
+        pathname = url.path
+        if url.query:
+            pathname += '?' + url.query
+        hostinfo = Host((hostname, port))
+        logger.debug('%s %s X%s', method.decode(), hostinfo.host, self.handler.socks_proxy or '-')
+        await self.open_connection(hostinfo.hostname, hostinfo.port)
+        headers = [header for header in headers if not header[0].startswith(b'proxy-')]
+        await send_request(self.remote[1], pathname.encode(), method, protocol, headers)
+        logger.debug('send request: %s %s %s', pathname, method, headers)
+        len_local, len_remote = await forward_pipes(self.reader, self.writer, *self.remote)
+        return len_local, len_remote
+
+class HTTPProxyHandler:
+    def __init__(self, socks_proxy=None):
+        self.socks_proxy = socks_proxy
+        self.connections = set()
+
+    async def __call__(self, reader, writer, feed=b''):
+        request = HTTPProxyRequest(self, reader, writer)
+        self.connections.add(request)
+        try:
+            await request.handle(feed)
+        finally:
+            self.connections.discard(request)
 
 if __name__ == '__main__':
-    from functools import partial
     from gera2ld.pyserve import start_server_asyncio, run_forever
-    logging.basicConfig(level=logging.INFO)
-    run_forever(start_server_asyncio(partial(handle, socks_proxy='socks5://127.0.0.1:2080'), ':4080'))
+    logging.basicConfig(level=logging.DEBUG)
+    run_forever(start_server_asyncio(HTTPProxyHandler('socks5://localhost:2020'), ':2100'))
