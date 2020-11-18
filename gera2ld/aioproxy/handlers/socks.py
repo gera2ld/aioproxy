850a23c8401fd803fab0a7bf5e6777079d895bc8
Gerald <gera2ld@live.com>
2020-11-18 16:55:03 +0800

    feat: enable UDP support

--- gera2ld/aioproxy/handlers/socks.py
+++ gera2ld/aioproxy/handlers/socks.py
@@ -1,12 +1,14 @@
 import logging
 import time
 from gera2ld.pyserve import Host
-from gera2ld.socks.server import SOCKS4Handler, SOCKS5Handler
+from gera2ld.socks.server import SOCKS4Handler, SOCKS5Handler, UDPRelayServer
 
 handler_map = {
     4: SOCKS4Handler,
     5: SOCKS5Handler,
 }
+udp_server = UDPRelayServer()
+
 
 async def handle(reader, writer, config, feed=b''):
     start_time = time.time()
@@ -14,8 +16,12 @@ async def handle(reader, writer, config, feed=b''):
         feed = await reader.readexactly(1)
     version, = feed
     Handler = handler_map[version]
-    handler = Handler(reader, writer, config, None)
+    handler = Handler(reader, writer, config, udp_server)
     _name, len_local, len_remote, _error = await handler.handle()
-    proxy = handler.config.get_proxy(host=handler.addr[0], port=handler.addr[1], hostname=handler.addr[0])
+    proxy = handler.config.get_proxy(host=handler.addr[0],
+                                     port=handler.addr[1],
+                                     hostname=handler.addr[0])
     proxy_log = ' X' + str(proxy) if proxy else ''
-    logging.info('SOCKS %s%s %.3fs <%d >%d', Host(handler.addr).host, proxy_log, time.time() - start_time, len_local, len_remote)
+    logging.info('SOCKS %s%s %.3fs <%d >%d',
+                 Host(handler.addr).host, proxy_log,
+                 time.time() - start_time, len_local, len_remote)
